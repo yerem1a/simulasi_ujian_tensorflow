7e873c910b03f6add2fd29e6c48bcbb6171857b9
yerem1a <57580438+yerem1a@users.noreply.github.com>
2022-07-18 11:05:35 +0700

    menambahkan problem C

---
+++ Problem C/Problem_C4.py
@@ -0,0 +1,84 @@
+# =======================================================================================================
+# PROBLEM C3
+#
+# Build a CNN based classifier for Cats vs Dogs dataset.
+# Your input layer should accept 150x150 with 3 bytes color as the input shape.
+# This is unlabeled data, use ImageDataGenerator to automatically label it.
+# Don't use lambda layers in your model.
+#
+# The dataset used in this problem is originally published in https://www.kaggle.com/c/dogs-vs-cats/data
+#
+# Desired accuracy and validation_accuracy > 72%
+# ========================================================================================================
+
+import tensorflow as tf
+import urllib.request
+import zipfile
+import tensorflow as tf
+import os
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+def solution_C4():
+    data_url = 'https://github.com/dicodingacademy/assets/raw/main/Simulation/machine_learning/sarcasm.json'
+    urllib.request.urlretrieve(data_url, 'sarcasm.json')
+
+    # DO NOT CHANGE THIS CODE
+    # Make sure you used all of these parameters or test may fail
+    vocab_size = 1000
+    embedding_dim = 16
+    max_length = 120
+    trunc_type = 'post'
+    padding_type = 'post'
+    oov_tok = "<OOV>"
+    training_size = 20000
+
+    sentences = []
+    labels = []
+    
+    with open("./sarcasm.json", 'r') as f:
+      datastore = json.load(f)
+
+    for item in datastore:
+      sentences.append(item['headline'])
+      labels.append(item['is_sarcastic'])
+
+    training_sentences = sentences[0:training_size]
+    testing_sentences = sentences[training_size:]
+    training_labels = labels[0:training_size]
+    testing_labels = labels[training_size:]
+
+    # Fit your tokenizer with training data
+    tokenizer =  Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+    tokenizer.fit_on_texts(training_sentences)
+    word_index = tokenizer.word_index
+
+    training_sequences = tokenizer.texts_to_sequences(training_sentences)
+    training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+    testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+    testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+    
+    training_labels = np.array(training_labels)
+    testing_labels = np.array(testing_labels)
+
+    model = tf.keras.Sequential([
+        # YOUR CODE HERE. DO not change the last layer or test may fail
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+        tf.keras.layers.GlobalMaxPooling1D(),
+        tf.keras.layers.Dense(6, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+    history = model.fit(training_padded, training_labels, epochs=20, validation_data=(testing_padded, testing_labels), verbose=2)
+
+    return model
+
+
+# The code below is to save your model as a .h5 file.
+# It will be saved automatically in your Submission folder.
+if __name__ == '__main__':
+    # DO NOT CHANGE THIS CODE
+    model = solution_C4()
+    model.save("model_C4.h5")
